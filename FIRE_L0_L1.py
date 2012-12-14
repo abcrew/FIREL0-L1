c247395f6c09fa8095502a4951e81d80429a973a
Brian Larsen <balarsen@lanl.gov>
2012-12-14 11:04:09 -0700

    FIRE_L0_L1.py: reads everything into lists, moving to arrays

--- FIRE_L0_L1.py
+++ FIRE_L0_L1.py
@@ -5,10 +5,15 @@ FIRE_L0_L1.py
 Convertor form the FIRE L0 packets to L1 data
 
 """
-
+# standard library includes (alphabetical)
+import datetime
+# dependency includes (alphabetical)
+import dateutil.parser as dup
+import numpy as np
 import spacepy.datamodel as dm
 
-datafile = 'tests/data/fire_data_121119_preship.rtf'
+
+
 
 def readL0(datafile):
     """
@@ -16,57 +21,67 @@ def readL0(datafile):
     """
     df = open(datafile, 'r')
     #ski9p the header
-    header = df.readline()
+    header = df.readline().strip()
     raw_data = df.readlines()
-    return [v.strip() for v in raw_data]
+    data = dm.SpaceData() # dictionary to hold the data
+    data.attrs['ReadDatetime'] = datetime.datetime.utcnow() #TODO think in UTC or local?  I like UTC
+    data.attrs['DataDatetime'] = dup.parse(header)
+    #TODO there will be MSU timestanps and all on the lines from BIRD, those need to be looked at
+    data['raw'] = [v.strip() for v in raw_data]
+    return data
 
 
-
-
-data_type = list()
-pkt_cntr = list()
-cmd_reg_val = list()
-cntrl_reg = list()
-d1hr = list()
-d2hr = list()
-hr0 = list()
-hr1 = list()
-hr2 = list()
-hr3 = list()
-hr4 = list()
-hr5 = list()
-
-
-for i in raw_data:
-    if(len(i) > 96):
-        data_type.append(int(i[0:2],16))
-        pkt_cntr.append(int(i[2:4],16))
-        cmd_reg_val.append(int(i[4:6],16))
-        cntrl_reg.append(int(i[6:8],16))
-        hr = list()
-        for k in range(6):
-            hr.append(int(i[24+k*2:26+k*2],16))
-        d1hr.append(hr)
-        hr = list()        
-        for k in range(6):
-            hr.append(int(i[36+k*2:38+k*2],16))
-        d2hr.append(hr)
+def parseL0(indata):
+    """
+    take in a SpaceData of raw L0 and parse it
+    return a SpaceData with the new info added
+    """
+    data_type = list()
+    pkt_cntr = list()
+    cmd_reg_val = list()
+    cntrl_reg = list()
+    d1hr = list()
+    d2hr = list()
+    hr0 = list()
+    hr1 = list()
+    hr2 = list()
+    hr3 = list()
+    hr4 = list()
+    hr5 = list()
     
-    
-    
-nlines = len(data_type)
 
-d = list()
+    for dat in indata['raw']:
+        if(len(dat) == 96):
+            data_type.append(int(dat[0:2],16))
+            pkt_cntr.append(int(dat[2:4],16))
+            cmd_reg_val.append(int(dat[4:6],16))
+            cntrl_reg.append(int(dat[6:8],16))
+            hr = list()
+            for chan in range(6):
+                hr.append(int(dat[24+chan*2:26+chan*2],16))
+            d1hr.append(hr)
+            hr = list()        
+            for chan in range(6):
+                hr.append(int(dat[36+chan*2:38+chan*2],16))
+            d2hr.append(hr)
+    
+    nlines = len(data_type)
+
+    d = list()
+
+    for j in range(int(nlines/16)):
+        data_str = dict(pkt_cntr=pkt_cntr[j*16+0:j*16+16], cmd_vals=cmd_reg_val[j*16+0:j*16+16],
+                        d1hr=d1hr[j*16+0:j*16+16], d2hr=d2hr[j*16+0:j*16+16])
+        d.append(data_str)
+    
+
+if __name__ == '__main__':
+    datafile = 'tests/data/fire_data_121119_preship.rtf'
+    d1 = readL0(datafile)
+    d2 = parseL0(d1)
 
 
-for j in range(int(nlines/16)):
-    data_str = dict(pkt_cntr=pkt_cntr[j*16+0:j*16+16], cmd_vals=cmd_reg_val[j*16+0:j*16+16],
-                    d1hr=d1hr[j*16+0:j*16+16], d2hr=d2hr[j*16+0:j*16+16])
-    d.append(data_str)
-    
-    
-    
-    
+
 
 """
 FIRE ICD definitions by byte:
