0d88d814f5320a96b599281002fe02e3115399f0
Brian Larsen <balarsen@gmail.com>
2013-07-26 22:15:43 -0600

    Request.py: finished Entry class

--- SciRequest/Request.py
+++ SciRequest/Request.py
@@ -3,23 +3,46 @@
 Class defines the format of the data passed back and forth from FIREBIRD
 ops to FIREBIRD science
 """
+from __future__ import division
 
 import datetime
+import warnings
 
-types = ['HIRES', 'CONTEXT', 'MICRO_BURST', 'CONTEXT', 'CONFIG', 'DATA_TIMES']
+warnings.simplefilter('always')
+
+typeDict = {'HIRES':{'dataPerBlock':2.94375},
+            'CONTEXT':{'dataPerBlock':1752},
+            'MICRO_BURST':{'dataPerBlock':584},
+            'CONFIG':{'dataPerBlock':54.48},
+            'DATA_TIMES':{'dataPerBlock':2000}} # This was a TBD
+secondsPerPage = 0.29
 
 class Entry(object):
     """
     class to hold a sinlge entry in a Request
     """
     def __init__(self, typ, date, duration, JAS=None):
-        if typ.upper() not in types:
-            raise(ValueError('Type: {0} not recognized, must be one of {1}'.format(typ.upper(), types)))
+        if typ.upper() not in typeDict:
+            raise(ValueError('Type: {0} not recognized, must be one of {1}'.format(typ.upper(), typeDict.keys())))
         self.typ = typ.upper()
         self.date = date
         self.duration = int(duration)
         self.JAS = JAS
-        self.downlinktime = 0.0 # to be filled with best guesses
+        self.downlinktime = None # to be filled by a calculation
+        self._calcDownlink()
+        if self.downlinktime > 60.0: # if too long
+            while self.downlinktime > 60.0: # just keep reducing it until it is ok
+                self.duration -= 1
+                self._calcDownlink()
+            warnings.warn("Downlink time was longer than allowed, duration shortened to {0}".format(self.duration))
+
+    def _calcDownlink(self, rate=19200):
+        """
+        given what we know about the duration of each 4k block calcualte how
+        long the downlink for the reqested data will take
+        """
+        ticks = self.duration/typeDict[self.typ]['dataPerBlock']
+        self.downlinktime = ticks*secondsPerPage
 
     @staticmethod
     def _timeSplit(dt):
