2501abc6b4ee52d1e1b3626be89e90b4e1c21e7d
Brian Larsen <balarsen@lanl.gov>
2013-12-25 16:34:06 -0900

    scripts/FIREBIRDcombine.py: script to properly combine L0 data files

---
+++ scripts/FIREBIRDcombine.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+Code to combine FIREBIRD data files changing the GRT time into a GRT datetime
+
+
+"""
+import datetime
+import os
+import sys
+
+from L0toL1 import packet
+
+def doIt(files):
+    """
+    read in the files and covert all the GRT to isoformat
+    """
+    ans = []
+    for f in files:
+        with open(f, 'r') as fp:
+            lines = fp.readlines()
+        ans.extend([packet.parseline(v, filename=f) for v in lines])
+    ans = sorted(ans)
+    return ans
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage: {0} infile [[infile] [infile] [...]]")
+        sys.exit(-1)
+    files = sys.argv[1:]
+    ans = doIt(files)
+    ext = files[0].split('-')[-1]
+    outname = datetime.date.today().isoformat() + '-' + ext
+    with open(outname, 'w') as fp:
+        fp.writelines(ans)
+    print("Wrote: {0}, {1} packets".format(os.path.abspath(outname), len(ans)))
+
+
+    
+
+    
--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from distutils.core import setup
 setup(name='FIREBIRD',
       version='1.0',
       packages=['DataSync', 'L0toL1', 'L1toL2', 'SciRequest'],
-      scripts=['scripts/FIRE_L0_L1.py', 'scripts/makeRequest.py']
+      scripts=['scripts/FIRE_L0_L1.py',
+               'scripts/makeRequest.py',
+               'scripts/FIREBIRDcombine.py'],
+      package_data={'DataSync' : ['*.conf'],
+                    'L1toL2' : ['*.cal']},
       )
